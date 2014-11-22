0a83d1f9f3d367796dc01b10fef20e5af72e2aa3
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2014-11-23 00:51:15 +0100

    just create a model of a person with a VCARD

---
+++ src/main/java/Tutorial01.java
@@ -0,0 +1,24 @@
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
+class Tutorial01 {
+    // some definitions
+    static String personURI    = "http://somewhere/RaphaelCazenaveLeveque";
+    static String fullName     = "Raphael Cazenave-Leveque";
+
+    public static void main (String args[]) {
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        // create the resource
+        Resource johnSmith = model.createResource(personURI);
+
+        // add the property
+        johnSmith.addProperty(VCARD.FN, fullName);
+
+        // show a representation of this model
+        System.out.println(model);
+    }
+}
