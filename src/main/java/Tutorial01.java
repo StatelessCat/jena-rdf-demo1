128f7ca0acf614aac29dc32f5c4b1ddb441c7166
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2014-11-23 01:29:03 +0100

    using blank node and property takes a resource as value

--- src/main/java/Tutorial01.java
+++ src/main/java/Tutorial01.java
@@ -6,17 +6,22 @@ import com.hp.hpl.jena.vocabulary.VCARD;
 class Tutorial01 {
     // some definitions
     static String personURI    = "http://somewhere/RaphaelCazenaveLeveque";
-    static String fullName     = "Raphael Cazenave-Leveque";
+    static String givenName    = "Raphael";
+    static String familyName   = "Cazenave-Leveque";
+    static String fullName     = givenName + " " + familyName;
 
     public static void main (String args[]) {
         // create an empty model
         Model model = ModelFactory.createDefaultModel();
 
         // create the resource
-        Resource raphaelCazenaveLeveque = model.createResource(personURI);
-
-        // add the property
-        raphaelCazenaveLeveque.addProperty(VCARD.N, fullName);
+        Resource raphaelCazenaveLeveque =
+                model.createResource(personURI)
+                        .addProperty(VCARD.FN, fullName)
+                        .addProperty(VCARD.N,
+                                model.createResource() // blank node
+                                        .addProperty(VCARD.Given, givenName)
+                                        .addProperty(VCARD.Family, familyName));
 
         // show a representation of this model
         System.out.println(model);
