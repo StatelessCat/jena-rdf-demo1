f9f99e28a2a2b7577f687a77e78278d87a2de3b2
Raphael Cazenave-Leveque <raphael.cazenave@gmail.com>
2014-11-23 01:51:43 +0100

    print out the predicate, subject and object of each statement

--- src/main/java/Tutorial01.java
+++ src/main/java/Tutorial01.java
@@ -1,6 +1,4 @@
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
 class Tutorial01 {
@@ -23,7 +21,30 @@ class Tutorial01 {
                                         .addProperty(VCARD.Given, givenName)
                                         .addProperty(VCARD.Family, familyName));
 
-        // show a representation of this model
-        System.out.println(model);
+        // list the statements in the Model
+        StmtIterator iter = model.listStatements();
+
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();  // get next statement
+            Resource  subject   = stmt.getSubject();     // get the subject
+            Property  predicate = stmt.getPredicate();   // get the predicate
+
+            // get the object
+            // the getObject() method returns an object typed as RDFNode,
+            // which is a common superclass of both Resource and Literal
+            RDFNode   object    = stmt.getObject();
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+
+            System.out.println(" .");
+        }
     }
 }
